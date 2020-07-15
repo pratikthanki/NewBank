7d47607c53427a439f1f2241ff177b0c9964dbb8
Peter Orisadipe <rotmoris@yahoo.com>
2020-07-15 10:48:40 +0100

    Added functionality to view customer detail, added sample data, and updated menu.

--- src/main/java/newbank/server/Customer.java
+++ src/main/java/newbank/server/Customer.java
@@ -1,13 +1,30 @@
 package newbank.server;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Customer {
 
     private ArrayList<Account> accounts;
+    private Date dob;
+	private String email;
+    private String name;
+    private String address;
+    private CustomerID customerID;
+    
 
-    public Customer() {
+    public String getCustomerID() {
+		return customerID.getKey();
+	}
+
+	public Customer() {
         accounts = new ArrayList<>();
+    }
+    
+	//Constructor 2: Create customer while providing CustomerID
+    public Customer(CustomerID customerID) {
+    	this(); //Customer()
+        this.customerID = customerID;
     }
 
     public ArrayList<Account> getAccounts(){
@@ -25,4 +42,58 @@ public class Customer {
     public void addAccount(Account account) {
         accounts.add(account);
     }
+    
+    public Date getDob() {
+		return this.dob;
+	}
+
+	public boolean setDob(Date dob) {
+		this.dob = dob;
+		return true;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public boolean setEmail(String email) {
+		this.email = email;
+		return true;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public boolean setName(String name) {
+		this.name = name;
+		return true;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public boolean setAddress(String address) {
+		this.address = address;
+		return true;
+	}
+	
+	public String getDetail() {
+		StringBuffer buffer = new StringBuffer();
+		
+		buffer.append("Customer Name: \t" + getName() + "\n");
+		buffer.append("Date of Birth: \t" + getDob() + "\n");
+		buffer.append("Email Address: \t" + getEmail() + "\n");
+		buffer.append("Address: \t" + getAddress() + "\n");
+		
+		return buffer.toString();
+	}
+	
+	public boolean updateDetail(String name, Date dob, String email, String address) {
+		if (setName(name) && setEmail(email) && setDob(dob) && setAddress(address) ) 
+			return true;
+		
+		return false;
+	}
 }
