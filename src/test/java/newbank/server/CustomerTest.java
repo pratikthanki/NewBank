8f71cdb456a7378738f2988fd0ffc08692b9a732
Peter Orisadipe <rotmoris@yahoo.com>
2020-07-20 18:05:11 +0100

    created tests for customer properties

--- src/test/java/newbank/server/CustomerTest.java
+++ src/test/java/newbank/server/CustomerTest.java
@@ -5,23 +5,63 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 public class CustomerTest extends TestCase {
     Account accountName;
     Customer customerName;
+    
 
     @Before
     public void setUp() {
         accountName = new Account("savings", 123);
         customerName = new Customer();
         customerName.addAccount(accountName);
+        
+        customerName.setEmail("test@gmail.com");
+        customerName.setAddress("6 Osapa, Lagos, Nigeria");
+        customerName.setName("Peter Test");
+        try {
+			customerName.setDob(new SimpleDateFormat("d M yyyy").parse("2 8 1912"));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+        
     }
 
     @Test
     public void testAccountsToString() {
         Assert.assertEquals("savings: 123.0", customerName.accountsToString());
     }
+    
+    @Test
+    public void testSetEmail() {
+        Assert.assertEquals("test@gmail.com", customerName.getEmail());
+    }
+    
+    @Test
+    public void testSetAddress() {
+        Assert.assertEquals("6 Osapa, Lagos, Nigeria", customerName.getAddress());
+    }
+    
+    @Test
+    public void testSetName() {
+        Assert.assertEquals("Peter Test", customerName.getName());
+    }
+    
+    @Test
+    public void testSetDob() {
+        try {
+			Assert.assertEquals(new SimpleDateFormat("d M yyyy").parse("2 8 1912"), customerName.getDob());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
 
     @Test
     public void testAddAccount() {
