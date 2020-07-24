7f8e2aae83017786ee1616aa8c8eff17f059cbd5
Pratik Thanki <pratikthanki1@gmail.com>
2020-07-24 16:06:00 +0100

    Fix failing test.

--- src/main/java/newbank/server/authentication/BasicAuthenticator.java
+++ src/main/java/newbank/server/authentication/BasicAuthenticator.java
@@ -1,26 +1,23 @@
 package newbank.server.authentication;
 
 import newbank.database.DatabaseClient;
-import newbank.server.Customer;
 import newbank.server.CustomerID;
 
 import java.net.Authenticator;
-import java.util.HashMap;
 
 
 public class BasicAuthenticator extends Authenticator {
     private String userName, password;
     private DatabaseClient databaseClient = new DatabaseClient();
-    private HashMap<String, Customer> customers;
 
     public BasicAuthenticator(final String userName, final String password) {
         this.userName = userName;
         this.password = password;
-        customers = databaseClient.getCustomers();
-
     }
 
     public CustomerID ValidateLogin() {
+        if (databaseClient.getCustomerByName(this.userName) == null) return null;
+
         String pwd = databaseClient.getCustomerByName(this.userName).getPassword();
         if (pwd.equals(this.password)) return new CustomerID(this.userName);
 
