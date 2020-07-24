5dd0df23d71ca9c6d5ea868127330cb46bf6fec5
SvetA95 <sanastasov13@gmail.com>
2020-07-24 15:26:02 +0100

    Code tidy

--- src/main/java/newbank/server/IPaymentHelper.java
+++ src/main/java/newbank/server/IPaymentHelper.java
@@ -58,5 +58,4 @@ public class IPaymentHelper implements IPayment {
 
 
 
-
 }
--- src/main/java/newbank/server/NewBankClientHandler.java
+++ src/main/java/newbank/server/NewBankClientHandler.java
@@ -129,5 +129,4 @@ public class NewBankClientHandler extends Thread {
             }
         } while (!menuItem.equals("9"));
     }
-
 }
